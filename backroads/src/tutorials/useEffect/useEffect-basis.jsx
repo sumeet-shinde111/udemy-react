3f4261ced052541de5a5d6703a3fe765929fd713
sumeetshinde <sumeetshinde111@gmail.com>
2026-05-02 17:12:20 +0530

    Multiple useEffects in a single function

--- backroads/src/tutorials/useEffect/useEffect-basis.jsx
+++ backroads/src/tutorials/useEffect/useEffect-basis.jsx
@@ -8,29 +8,47 @@ Cases of dependency array
 2) if we pass dependency array as empty it will only runs on initial render
 3) if we pass value to dependency array it will run useEffect when the value is changed.
 
+We can have multiple useEffect but ideally we should not add more than 1.
 */
 
 const UseEffectBasic = ()=>{
 
-    const [value,setValue] = useState(0);
+    const [value1,setValue1] = useState(0);
+    const [value2,setValue2] = useState(0);
 
-    const hello = ()=>{
-        console.log("hello..")
-    }
-    hello();
+
 
     useEffect(()=>{
-        console.log("This useEffct and hello() will run on each render on click of the button...")
+        // This will run on each render as no dependecy array is passed
+        console.log("This useEffct on every render...")
     })
 
-    const increment = ()=>{
-        setValue(value+1);
+      useEffect(()=>{
+        // This will run when value 1 is incremented..
+        console.log("This useEffct for first button...")
+    },[value1])
+    
+    useEffect(()=>{
+        // This will run when value 2 is incremented..
+        console.log("useEffect for second button...")
+    },[value2])
+
+    const increment1 = ()=>{
+        setValue1(value1+1);
+    }
+
+      const increment2 = ()=>{
+        setValue2(value2+1);
     }
 
     return(
         <>
-            <h1>Value : {value}</h1>
-            <button onClick={increment}>Click me</button>
+            <h1>Value1 : {value1}</h1>
+            <h1>Value2 : {value2}</h1>
+
+            <button onClick={increment1}>Click me 1</button>
+            <button onClick={increment2}>Click me 2</button>
+
         </>
     )
 }
